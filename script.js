9ea4732ff3270d084f7643d3ed080c5e5ded11f8
Justin Hoffman <justinhoffman193@gmail.com>
2021-04-11 21:57:48 -0500

    Added Character functions to produce random characters

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 // Assignment code here
 
+function RandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26)+ 97);
+}
+// console.log(RandomLower());
+
+function RandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26)+ 65);
+}
+// console.log(RandomUpper());
+
+function RandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10)+ 48);
+}
+// console.log(RandomNumber());
+
+function RandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>,.';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+
+}
+    console.log(RandomSymbol());
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +39,8 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
